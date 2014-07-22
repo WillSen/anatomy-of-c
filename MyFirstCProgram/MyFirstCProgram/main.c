5b751464107666aacf0fca69542f0fda36ab30b3
Will Sentance <william.sentance@gmail.com>
2014-07-21 20:18:13 -0800

    initial commit

---
+++ MyFirstCProgram/MyFirstCProgram/main.c
@@ -0,0 +1,44 @@
+//
+//  main.c
+//  MyFirstCProgram
+//
+//  Created by turner on 9/26/13.
+//  Copyright (c) 2013 treehouse. All rights reserved.
+//
+
+#include <stdio.h>
+
+int funky_math (int a, int b);
+
+int main()
+{
+    int days_in_a_week = 7;
+    float cm_to_in = 2.54;
+    
+    char the_w;
+    the_w = 'W';
+    
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("%d days in a week.\n", days_in_a_week);
+    printf("%f cm to in", cm_to_in);
+    printf("%c \n", the_w);
+    
+    char letters[] = {'a','b','c'};
+    int w = 0;
+    while (w < 3) {
+        printf("letter at %d is %c\n", w, letters[w]);
+        ++w;
+    }
+    
+    int foo = 20;
+    int barr = 15;
+    int output = funky_math(foo, barr);
+    printf("%d", output);
+    
+    return 0;
+}
+
+int funky_math (int a, int b){
+    return a + b + 242;
+};
